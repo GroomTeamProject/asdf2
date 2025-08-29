51025de7ac98e5a499cc536a5b0e4dbcc85b8726
wsk0715 <leindey@gmail.com>
2025-08-29 17:29:50 +0900

    chore: 사용하지 않는 코드 정리

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
+
 // 라우트 경로 추가
 const routes = [
   { path: '/', component: Home },
+]
 
 const router = createRouter({
   history: createWebHistory(),
