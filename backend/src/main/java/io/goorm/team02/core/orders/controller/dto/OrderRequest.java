5aa7c0bcd68c39c8bd8684407affe840cdf2ca26
wsk221e <wsk221e@gmail.com>
2025-08-29 15:12:18 +0900

    feat: 데이터베이스 스키마 및 엔티티 수정 (Orders)

---
+++ backend/src/main/java/io/goorm/team02/core/orders/controller/dto/OrderRequest.java
@@ -0,0 +1,16 @@
+package io.goorm.team02.core.orders.controller.dto;
+
+import io.goorm.team02.core.orders.domain.Order;
+
+public record OrderRequest(
+		String orderMemo
+) {
+
+	public Order toEntity() {
+		Order order = new Order();
+		order.setOrderMemo(this.orderMemo);
+
+		return order;
+	}
+
+}
--- backend/src/main/java/io/goorm/team02/core/orders/repository/OrderRepository.java
+++ backend/src/main/java/io/goorm/team02/core/orders/repository/OrderRepository.java
@@ -1,6 +1,6 @@
-package io.goorm.team02.core.order.repository;
+package io.goorm.team02.core.orders.repository;
 
-import io.goorm.team02.core.order.domain.Order;
+import io.goorm.team02.core.orders.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
