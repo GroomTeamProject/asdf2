168c356fe269f4063c6c1428372679468b4255ed
wsk221e <wsk221e@gmail.com>
2025-09-13 14:50:37 +0900

    chore: 테스트 프로파일 설정 방식 변경

--- backend/src/test/java/io/goorm/team02/core/CoreApplicationTests.java
+++ backend/src/test/java/io/goorm/team02/core/CoreApplicationTests.java
@@ -2,11 +2,9 @@ package io.goorm.team02.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class CoreApplicationTests {
+class CoreApplicationTests extends TestEnv {
 
 	@Test
 	void contextLoads() {
