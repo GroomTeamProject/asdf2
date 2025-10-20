56178b4ebbadc66672fc95c4ec755873a87ede52
wsk221e <wsk221e@gmail.com>
2025-10-20 10:10:13 +0900

    fix: 빌드 오류 수정

--- msa/user-service/src/test/java/io/goorm/team02/users/TestEnv.java
+++ msa/user-service/src/test/java/io/goorm/team02/users/TestEnv.java
@@ -1,4 +1,4 @@
-package io.goorm.team02.core;
+package io.goorm.team02.users;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
--- msa/user-service/src/test/java/io/goorm/team02/users/UserApplicationTests.java
+++ msa/user-service/src/test/java/io/goorm/team02/users/UserApplicationTests.java
@@ -1,10 +1,10 @@
-package io.goorm.team02.core;
+package io.goorm.team02.users;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CoreApplicationTests extends TestEnv {
+class UserApplicationTests extends TestEnv {
 
 	@Test
 	void contextLoads() {
