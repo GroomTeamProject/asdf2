a2861cedc14e8d0057c0627b779a3d838fcd8ef9
anhwakim <anhwakim@anhwakims-MacBook-Pro.local>
2025-11-24 15:09:29 +0900

    Feature/msa_rider: orderClient update

--- msa/delivery-service/src/main/java/io/goorm/team02/core/delivery/client/OrderServiceClient.java
+++ msa/delivery-service/src/main/java/io/goorm/team02/core/delivery/client/OrderServiceClient.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//  2025-11-24
+//  2025-11-24: GetMapping
 @FeignClient(name = "order-service", url = "${msa.gateway.url}")
 public interface OrderServiceClient {
 
-    @GetMapping("/delivery/{orderId}")
+    @GetMapping("/api/orders/delivery/{orderId}")
     OrderResponseForDelivery getOrderDetailForDelivery(@PathVariable("orderId") Long orderId);
 }
